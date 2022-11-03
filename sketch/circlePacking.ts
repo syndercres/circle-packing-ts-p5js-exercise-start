42503731f3ad07f7304e276f5715a416e7524881
nbogie <neillbogie@googlemail.com>
2022-11-03 01:06:09 +0000

    clarifications. e.g. Circle is now CircleData

--- sketch/circlePacking.ts
+++ sketch/circlePacking.ts
@@ -1,5 +1,5 @@
-interface Circle {
-    pos: Position;
+interface CircleData {
+    position: Position;
     radius: number;
 }
 interface Position {
@@ -7,17 +7,31 @@ interface Position {
     y: number;
 }
 
-function calculatePackedCircles(areaWidth: number, areaHeight: number): Circle[] {
-    //TODO: you need to implement this function properly!  
-    //These are just a couple of random circles, with no consideration yet for overlap.
-    return [
-        { pos: { x: 300, y: 300 }, radius: 100 },
-        { pos: { x: random(0, areaWidth), y: random(0, areaHeight) }, radius: 40 },
-    ];
+/**
+ * Creates and returns an array of Circle objects which, when visualised, do not overlap each other.
+ * @param areaWidth the width of the area within which circles may be generated
+ * @param areaHeight the height of the area within which circles may be generated
+ * @return an array of generated Circle data objects
+ */
+function calculatePackedCircles(
+    areaWidth: number,
+    areaHeight: number
+): CircleData[] {
+    //TODO: you need to implement this function properly!
+    //These are just a couple of random circles, with no consideration yet for avoiding overlap.
+    const circleDataOne: CircleData = {
+        position: { x: 300, y: 300 },
+        radius: 100,
+    };
+    const circleDataTwo: CircleData = {
+        position: { x: random(0, areaWidth), y: random(0, areaHeight) },
+        radius: 40,
+    };
+    return [circleDataOne, circleDataTwo];
 }
 
 /** Returns the distance between two given positions.
-    This function doesn't require p5.js 
+    This function doesn't require the p5.js library.
  */
 function distance(p1: Position, p2: Position): number {
     const x = p1.x - p2.x;
--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,26 +1,26 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  noLoop();
+    createCanvas(windowWidth, windowHeight);
+    noLoop();
 }
 
 function draw() {
-  background('white');
+    background("white");
 
-  const circlesArray = calculatePackedCircles(width, height);
+    const circlesArray: CircleData[] = calculatePackedCircles(width, height);
 
-  for (const c of circlesArray) {
-    drawCircle(c);
-  }
+    for (const c of circlesArray) {
+        drawCircle(c);
+    }
 }
 
-function drawCircle(c: Circle) {
-  const shade = random(50, 100);
-  fill(shade);
-  noStroke();
-  circle(c.pos.x, c.pos.y, c.radius * 2);
+function drawCircle(c: CircleData) {
+    const shade = random(50, 100);
+    fill(shade);
+    noStroke();
+    circle(c.position.x, c.position.y, c.radius * 2);
 }
 
-// If user clicks, draw() will be called again (eventually)
+// If user clicks, draw() will be called again (indirectly)
 function mousePressed() {
-  redraw();
+    redraw();
 }
